b84e9e216137bce8853f56a3fa502128172a3486
Maria Helena <mah.helenis@gmail.com>
2026-08-14 10:02:49 -0300

    chore(backend): add backend Java/Spring Boot boilerplate

---
+++ backend/src/main/java/com/branch_master/ecovolt360/Ecovolt360Application.java
@@ -0,0 +1,13 @@
+package com.branch_master.ecovolt360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecovolt360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecovolt360Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/branch_master/ecovolt360/Ecovolt360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.branch_master.ecovolt360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecovolt360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
