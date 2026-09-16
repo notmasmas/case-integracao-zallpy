bf94830acddead2445f97b0209853d6531206c36
Maria Helena <mah.helenis@gmail.com>
2026-09-16 16:00:15 -0300

    feat: add migrations for addresses, users, projects and tickets

---
+++ backend/src/main/resources/db/migration/V4__create-table-tickets.sql
@@ -0,0 +1,14 @@
+CREATE TABLE tickets (
+    id uuid,
+    customer_id uuid REFERENCES users(id),
+    support_id uuid REFERENCES users(id),
+    project_id uuid REFERENCES projects(id),
+    title VARCHAR(50),
+    description VARCHAR(150),
+    category VARCHAR(50),
+    status VARCHAR(50),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+
+    PRIMARY KEY (id)
+)
